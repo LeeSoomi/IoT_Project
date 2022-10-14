49e7e80fa1835042a5bba8912e0b8a7b9fc388b6
LeeSoomi <70505715+LeeSoomi@users.noreply.github.com>
2022-10-14 18:56:21 +0900

    Update code.ino

--- Baek_SH/code.ino
+++ Baek_SH/code.ino
@@ -81,7 +81,6 @@ void readSensors() {
   pressure = carrier.Pressure.readPressure();
 }
 
-
 void sensor_Data() {
   // Co2 센서값 읽기
   int sensorValue = analogRead(co2_pin);
@@ -97,7 +96,6 @@ void sensor_Data() {
     co2_alert_status = 1  ;
   }
   
-
   // Gas 센서값 읽기
   gas = analogRead(gas_pin) ;
  
@@ -108,7 +106,6 @@ void sensor_Data() {
   else{
     gas_alert_status = 1  ;
   }
-
 
   // 미세먼지센서 값 계산
   digitalWrite(ledPower,LOW);                
@@ -125,18 +122,15 @@ void sensor_Data() {
   dustDensity = (0.17 * calcVoltage - 0.1) *1000;
   dust = dustDensity + 100 ;
 
-
   // 미세먼지값이 기준치 이상이면 화면에 LED색 바꾸기
   if(dust >= 150){
       dust_alert_status = 0 ;
     }
   else{
     dust_alert_status = 1  ;
-    }
-  
+    }  
   delay(1000);
 }
-
 
 void displayData() {
   //MKR IoT Carrier의 내장센서인 터치센서를 누르면 스크린에 온도, 습도, 기압을 보여준다.
